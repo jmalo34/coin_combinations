cc716148493c2f5c3b8fdff7e20f1b76d8abbeaa
Jenna Maloney <jmalo@uw.edu>
2015-11-30 15:27:55 -0800

    make all 5 tests pass

--- src/Coin.php
+++ src/Coin.php
@@ -1,81 +1,13 @@
 <?php
     class Coin
     {
-        // private $pennies;
-        // private $nickels;
-        // private $dimes;
-        // private $quarters;
-        //
-        // function __construct($pennies, $nickels, $dimes, $quarters)
-        // {
-        //     $this->pennies = $pennies;
-        //     $this->nickels = $nickels;
-        //     $this->dimes = $dimes;
-        //     $this->quarters = $quarters;
-        // }
-        //
-        // function getPennies()
-        // {
-        //     return $this->pennies;
-        // }
-        //
-        // function setPennies($new_pennies)
-        // {
-        //     $this->pennies = $new_pennies;
-        // }
-        //
-        // function getNickels()
-        // {
-        //     return $this->nickels;
-        // }
-        //
-        // function setNickels($new_nickels)
-        // {
-        //     $this->nickels = $new_nickels;
-        // }
-        //
-        // function getDimes()
-        // {
-        //     return $this->dimes;
-        // }
-        //
-        // function setDimes($new_dimes)
-        // {
-        //     $this->dimes = $new_dimes;
-        // }
-        //
-        // function getQuarters()
-        // {
-        //     return $this->quarters;
-        // }
-        //
-        // function setQuarters($new_quarters)
-        // {
-        //     $this->quarters = $new_quarters;
-        // }
-
         function changeDue($amount)
         {
-            // if (($amount % 25) == 0)
-            // {
-            //     $quarters = ($amount / 25);
-            // }
-            // elseif (($amount % 25) > 0)
-            // {
-            //     $quarters = (($amount - ($amount % 25)) / 25);
-            //     $amount = ($amount % 25);
-            //
-            // }
-            // else if (($amount % 10) == 0)
-            // {
-            //     $dimes = ($amount / 10);
-            // }
-            // elseif (($amount % 10) > 0)
-            // {
-            //     $dimes = (($amount = ($amount % 10)) / 10);
-            //     if ((;
-            //     $nickels =
-            // }
+            if ($amount >= 100)
+            {
+                return 'the amount you entered is in excess of 1 whole dollar';
+            }
+
             $quarters = (($amount - ($amount % 25)) / 25);
             $amount = ($amount % 25);
 
--- tests/CoinTest.php
+++ tests/CoinTest.php
@@ -41,31 +41,31 @@
             //Assert
             $this->assertEquals('0 quarters, 1 dimes, 1 nickels, 3 pennies', $result);
         }
-        //
-        // function test_change_quarter()
-        // {
-        //     //Arrange
-        //     $test_change = new Coin();
-        //     $input =;
-        //
-        //     //Act
-        //     $result = $test_change->changeDue($input);
-        //
-        //     //Assert
-        //     $this->assertEquals(.., $result);
-        // }
-        //
-        // function test_change_dollar()
-        // {
-        //     //Arrange
-        //     $test_change = new Coin();
-        //     $input =;
-        //
-        //     //Act
-        //     $result = $test_change->changeDue($input);
-        //
-        //     //Assert
-        //     $this->assertEquals(.., $result);
-        // }
+
+        function test_change_quarter()
+        {
+            //Arrange
+            $test_change = new Coin;
+            $input = 42;
+
+            //Act
+            $result = $test_change->changeDue($input);
+
+            //Assert
+            $this->assertEquals('1 quarters, 1 dimes, 1 nickels, 2 pennies', $result);
+        }
+
+        function test_change_dollar()
+        {
+            //Arrange
+            $test_change = new Coin;
+            $input = 101;
+
+            //Act
+            $result = $test_change->changeDue($input);
+
+            //Assert
+            $this->assertEquals('the amount you entered is in excess of 1 whole dollar', $result);
+        }
     }
  ?>
