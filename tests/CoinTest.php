cfcba66283066784293371c7f946181075758c34
Jenna Maloney <jmalo@uw.edu>
2015-11-30 15:24:22 -0800

    make first 3 tests pass

--- tests/CoinTest.php
+++ tests/CoinTest.php
@@ -29,18 +29,18 @@
             $this->assertEquals('0 quarters, 0 dimes, 1 nickels, 4 pennies', $result);
         }
 
-        // function test_change_dime()
-        // {
-        //     //Arrange
-        //     $test_change = new Coin();
-        //     $input =;
-        //
-        //     //Act
-        //     $result = $test_change->changeDue($input);
-        //
-        //     //Assert
-        //     $this->assertEquals(.., $result);
-        // }
+        function test_change_dime()
+        {
+            //Arrange
+            $test_change = new Coin;
+            $input = 18;
+
+            //Act
+            $result = $test_change->changeDue($input);
+
+            //Assert
+            $this->assertEquals('0 quarters, 1 dimes, 1 nickels, 3 pennies', $result);
+        }
         //
         // function test_change_quarter()
         // {
